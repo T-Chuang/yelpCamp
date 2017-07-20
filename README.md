a550b3c9649650641913c90d6298d702ca39447d
Theodore Chuang <chuangtheodore@gmail.com>
2017-07-20 23:27:38 +1000

    Added basic navbar UI. Basic form styling

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-=================================================
-                   Yelp Camp
-=================================================
+# =================================================
+#                   Yelp Camp
+# =================================================
 
+## Summary:
 The objective of this app is to help users find their next campground.
+
+## Features:
+* Users can browser through database of campgrounds
+* Users can add new campgrounds to the database
+
+## Built with:
+Express, Bootstrap
--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.set('view engine', 'ejs')
 let campgrounds = [
     {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?dpr=1&auto=format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop="},
     {name: "Baker's Cove", image: "https://images.unsplash.com/photo-1414016642750-7fdd78dc33d9?dpr=1&auto=format&fit=crop&w=1199&h=801&q=80&cs=tinysrgb&crop="},
+    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?dpr=1&auto=format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop="},
+    {name: "Baker's Cove", image: "https://images.unsplash.com/photo-1414016642750-7fdd78dc33d9?dpr=1&auto=format&fit=crop&w=1199&h=801&q=80&cs=tinysrgb&crop="},
+    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?dpr=1&auto=format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop="},
+    {name: "Baker's Cove", image: "https://images.unsplash.com/photo-1414016642750-7fdd78dc33d9?dpr=1&auto=format&fit=crop&w=1199&h=801&q=80&cs=tinysrgb&crop="},
+    {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?dpr=1&auto=format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop="},
+    {name: "Baker's Cove", image: "https://images.unsplash.com/photo-1414016642750-7fdd78dc33d9?dpr=1&auto=format&fit=crop&w=1199&h=801&q=80&cs=tinysrgb&crop="},
     {name: "King's Landing", image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?dpr=1&auto=format&fit=crop&w=1199&h=911&q=80&cs=tinysrgb&crop="}
 ]
 
