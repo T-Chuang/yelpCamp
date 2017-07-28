09468321f7abe1d14e8420ec79d970653b7b3f38
Theodore Chuang <chuangtheodore@gmail.com>
2017-07-28 17:09:49 +1000

    Auth: created Register and Login - form and POST logic

--- app.js
+++ app.js
@@ -123,6 +123,38 @@ app.post('/campgrounds/:id/comments', function(req, res){
 })
 
 
+//////////////  AUTH ROUTES  \\\\\\\\\\\\\\\
+
+// Registeration - show form
+app.get('/register', function(req, res){
+    res.render('register')
+})
+// Registeration - logic to add user to DB
+app.post('/register', function(req, res){
+    const newUser = new User({username: req.body.username})
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err)
+            return res.render('register')
+        }
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('campgrounds')
+        })
+    })
+})
+
+// Login - show form
+app.get('/login', function(req, res){
+    res.render('login')
+})
+// Login - checks against existing users
+app.post('/login', passport.authenticate('local',
+    {
+        successRedirect: '/campgrounds',
+        failuerRedirect: '/login'
+    }), function(req, res){
+})
+
 app.get('*', function(req, res){
     res.send('Oops, pages does not exist')
 })
